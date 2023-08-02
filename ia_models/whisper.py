4cbb8f13719bb963c3dfbf9f83a457bf034cac7d
villa121 <sebastian.franco@utp.edu.co>
2023-08-02 17:57:16 -0500

    about to make whisper work

--- ia_models/whisper.py
+++ ia_models/whisper.py
@@ -1,12 +1,6 @@
 import openai
 
-from pydub import AudioSegment
-
-
-
-invoice = AudioSegment.from_file("media/voice_note.ogg", "ogg")
-invoice.export("media/voice_note.mp3", format="mp3")
-
-
-# audio_file= open("/path/to/file/audio.mp3", "rb")
-# transcript = openai.Audio.transcribe("whisper-1", audio_file)
+def transcribe_audio():
+    audio_file= open("/media/voice_note.mp3", "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    return transcript
